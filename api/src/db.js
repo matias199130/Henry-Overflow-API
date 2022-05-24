e33e329fd13282a572404e145ecc8136569a9037
Alfredo Egremy <65094180+Egremy-quark@users.noreply.github.com>
2022-05-24 10:04:20 -0500

    henryOverflow: creación de la db

--- api/src/db.js
+++ api/src/db.js
@@ -3,35 +3,35 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST, DB_NAME} = process.env;
+    DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
 let sequelize =
-  process.env.NODE_ENV === "production"
-    ? new Sequelize({
-        database: DB_NAME,
-        dialect: "postgres",
-        host: DB_HOST,
-        port: 5432,
-        username: DB_USER,
-        password: DB_PASSWORD,
-        pool: {
-          max: 3,
-          min: 1,
-          idle: 10000,
-        },
-        dialectOptions: {
-          ssl: {
-            require: true,
-            // Ref.: https://github.com/brianc/node-postgres/issues/2009
-            rejectUnauthorized: false,
-          },
-          keepAlive: true,
-        },
-        ssl: true,
-      })
-    : new Sequelize(
-        `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/pokemon`,
-        { logging: false, native: false }
-      );
+    process.env.NODE_ENV === "production"
+        ? new Sequelize({
+            database: DB_NAME,
+            dialect: "postgres",
+            host: DB_HOST,
+            port: 5432,
+            username: DB_USER,
+            password: DB_PASSWORD,
+            pool: {
+                max: 3,
+                min: 1,
+                idle: 10000,
+            },
+            dialectOptions: {
+                ssl: {
+                    require: true,
+                    // Ref.: https://github.com/brianc/node-postgres/issues/2009
+                    rejectUnauthorized: false,
+                },
+                keepAlive: true,
+            },
+            ssl: true,
+        })
+        : new Sequelize(
+            `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/henryoverflow`,
+            { logging: false, native: false }
+        );
 // const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/pokemon`, {
 //   logging: false, // set to console.log to see the raw SQL queries
 //   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
@@ -42,10 +42,10 @@ const modelDefiners = [];
 
 // Leemos todos los archivos de la carpeta Models, los requerimos y agregamos al arreglo modelDefiners
 fs.readdirSync(path.join(__dirname, '/models'))
-  .filter((file) => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
-  .forEach((file) => {
-    modelDefiners.push(require(path.join(__dirname, '/models', file)));
-  });
+    .filter((file) => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
+    .forEach((file) => {
+        modelDefiners.push(require(path.join(__dirname, '/models', file)));
+    });
 
 // Injectamos la conexion (sequelize) a todos los modelos
 modelDefiners.forEach(model => model(sequelize));
@@ -64,6 +64,6 @@ const { } = sequelize.models;
 
 
 module.exports = {
-  ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
-  conn: sequelize,     // para importart la conexión { conn } = require('./db.js');
+    ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
+    conn: sequelize,     // para importart la conexión { conn } = require('./db.js');
 };
