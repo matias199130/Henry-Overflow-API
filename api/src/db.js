74170811851cc1777e5de3987a495c3350328e60
Alfredo Egremy <65094180+Egremy-quark@users.noreply.github.com>
2022-05-24 16:12:40 -0500

    db: relacioon belongs to

--- api/src/db.js
+++ api/src/db.js
@@ -56,12 +56,14 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { } = sequelize.models;
+const { Post, Tag, User } = sequelize.models;
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
+Post.belongsToMany(Tag, { through: "posts_tags" });
+Tag.belongsToMany(Post, { through: "posts_tags" });
 
-
+User.hasOne()
 
 module.exports = {
     ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
