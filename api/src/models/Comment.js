ac7b4805e756dc084c1d79ec23a7cec6a3983d7d
AllanNara <allannara@outlook.com>
2022-05-24 17:52:04 -0300

    change id model tag

--- api/src/models/Comment.js
+++ api/src/models/Comment.js
@@ -3,20 +3,21 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
     // defino el modelo
     sequelize.define('comment', {
-      id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        unique: true
-      },
-      message: {
-        type: DataTypes.TEXT,
-        unique: true,
-        allowNull: false
-      },
-      rating: {
-        type: DataTypes.FLOAT,
-        defaultValue: 0
-      }
+        id: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            primaryKey: true,
+            allowNull: false,
+        },
+        message: {
+            type: DataTypes.TEXT,
+            unique: true,
+            allowNull: false
+        },
+        rating: {
+            type: DataTypes.FLOAT,
+            defaultValue: 0
+        }
     },
     {
       timestamps: false
