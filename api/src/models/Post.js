ea1fe4f16b088c64ea35d14f4e286b9ed99a05b9
Ayilk <srangel.h92@gmail.com>
2022-05-24 15:43:13 -0500

    Añadiendo modelo post

---
+++ api/src/models/Post.js
@@ -0,0 +1,26 @@
+module.exports = (sequelize) => {
+    // defino el modelo
+    sequelize.define('post', {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+        allowNull: false,
+      },
+      title:{
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      message: {
+        type: DataTypes.TEXT,
+        allowNull: false
+      },
+      rating: {
+        type: DataTypes.STRING,
+        unique: true
+      }
+    },
+    {
+      timestamps: true
+    });
+  };
