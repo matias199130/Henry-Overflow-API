53ee40d59e5abe7bef04407e6d5a59735aa014b2
Ayilk <srangel.h92@gmail.com>
2022-05-25 15:00:07 -0500

    Trabajando en rutas user

---
+++ api/src/controllers/Users.js
@@ -0,0 +1,72 @@
+const { Post, Tag, User, Comment} = require('../db');
+
+const getUser = async(req, res, next) => {
+    const id = req.params.id;
+    const {first_name, last_name} = req.query;
+   
+    try {
+        const response = await User.findAll()
+        if(id){
+            let userId = response.filter(el => el.id == id);
+            userId.length ? res.status(200).send(userId) : res.status(400).send("user not found")
+        } 
+        // if(first_name && last_name){
+        //     let userName = response.filter(el => el.first_name.toLowerCase().includes(first_name.toLowerCase()));
+        //     let userLastName = response.filter(el => el.last_name.toLowerCase().includes(last_name.toLowerCase()));
+        //     if(userName === userLastName){
+        //       userName? res.send(userName) : res.status(400).send("User not found")
+        //     }
+        //     let fullName = userName.concat(userLastName);
+        //     fullName.length ? res.send(fullName) : res.status(400).send("User not found")
+        // } 
+        if(first_name){
+            let userName = response.filter(el => el.first_name.toLowerCase().includes(first_name.toLowerCase()));
+            userName.length ? res.send(userName) : res.status(400).send("User not found")
+        }
+        if(last_name){
+            let lastName = response.filter(el => el.last_name.toLowerCase().includes(last_name.toLowerCase()));
+            lastName.length ? res.send(lastName) : res.status(400).send("User not found")
+        }
+        res.json(response)
+    } catch (error) {
+        next(error)
+    }
+}
+
+
+const postUser = async(req, res, next) => {
+    try {
+        const newUser = await User.create(req.body);
+        res.json(newUser)
+    } catch (error) {
+        next(error)
+    }
+}
+
+const updateUser = (req, res, next) => {
+    const id = req.params.id;
+    const {firs_name, last_name, email, password, rating, badges, isAdmin, role, twitter, github, portfolio} = req.body;
+    return User.update(
+        {firs_name, last_name, email, password, rating, badges, isAdmin, role, twitter, github, portfolio},{
+            where: {id},  raw : true 
+        },
+    ).then(updatedUser => res.send(updatedUser))
+    .catch(error => next(error))
+}
+
+const deleteUser = (req, res, next) => {
+    const id = req. params.id;
+    return User.destroy({
+        where: {
+            id
+        }
+    }).then(() => {res.status(200).send("User deleted successfully")})
+    .catch(error => next(error))
+}
+
+module.exports = {
+    getUser,
+    postUser,
+    updateUser,
+    deleteUser
+}
