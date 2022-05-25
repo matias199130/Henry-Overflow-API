cd08faa9f2629bbad566fbf27201501f190b119b
Ayilk <srangel.h92@gmail.com>
2022-05-25 17:14:27 -0500

    cambio

--- api/src/controllers/Comment.js
+++ api/src/controllers/Comment.js
@@ -1,5 +1,33 @@
 const { Post, Tag, User, Comment} = require('../db');
 
+const getComment = (req, res, next) => {
+    const id = req.params.id;
+
+         Comment.findAll({
+            include: [
+                {
+                model: Post,
+                attributes: ["title", "id"],
+                through: {
+                    attributes: []
+                }
+                },                
+                {                    
+                model: User,
+                attributes: ["first_name", "last_name", "id"],                 
+                }       
+            ]
+        }).then(comment => {
+            if(id){
+                let commentId = comment.filter(el => el.id == id);
+                commentId.length ? res.status(200).send(commentId) : res.status(400).send("Comment not found")
+            } 
+            
+           return  res.send(comment)})
+        .catch(error => next(error))
+    
+}
+
 
 const updateComment = (req, res, next) => {
     const id = req.params.id;
