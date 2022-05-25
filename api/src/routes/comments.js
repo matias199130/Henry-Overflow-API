a3d02f66b813e5c3c832fc7dc672b6f9f47c0daf
AllanNara <allannara@outlook.com>
2022-05-25 15:26:30 -0300

    fixed route comments

--- api/src/routes/comments.js
+++ api/src/routes/comments.js
@@ -11,16 +11,16 @@ const { Comment, Post, User } = require('../db')
 
 /* 
 Entonces extraigo el ID del post por params, y de alli saco el
-post del que pertenece y desde el post al que pertenece tambien saco el usuario
+
+post del que pertenece y desde el post al que pertenece tambien saco el usuario ----> ERROR!!
+//////EL ID QUE TAMBIEN TENGO QUE RECIBIR ES EL DEL USER QUE HIZO EL COMENTARIO, NO EL USER QUE HIZO EL POSTEO
 Alli mismo hago la relacion con estas dos entidades(POST Y USER) asi queda guardada esta informacion
 Devuelvo el mensaje para verificar
 */
-router.post('/:idPost', async(req, res, next) => {
-    const { idPost } = req.params
+router.post('/:idPost/:idUser', async(req, res, next) => {
+    const { idPost, idUser } = req.params
     try {
         const createdInPost = await Post.findByPk(idPost, { include: [User] })
-
-        let idUser = createdInPost.toJSON().user.id
         const createdBy = await User.findByPk(idUser)
 
         const newComment = await Comment.create(req.body);
