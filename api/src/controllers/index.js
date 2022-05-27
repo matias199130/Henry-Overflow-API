713ef01520ba0ce72dee696509958461906656db
AllanNara <allannara@outlook.com>
2022-05-27 17:42:43 -0300

    fixed relations tags-modules

--- api/src/controllers/index.js
+++ api/src/controllers/index.js
@@ -159,7 +159,8 @@ async function relationsModulesTags() {
             { name: "Base de Datos" },
             { name: "Modelos" },
             { name: "CRUD" },
-            { name: "Autenticacion" }
+            { name: "Autenticacion" },
+            { name: "ORM" }
         ],
     },
   });
