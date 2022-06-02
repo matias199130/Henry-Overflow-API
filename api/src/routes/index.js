36eb42a08be9a3128a8ec23b2793e909456a1be4
reydez <icyreyeslod@gmail.com>
2022-06-01 21:34:40 -0500

    github login test

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,26 +1,27 @@
-const { Router } = require('express');
+const { Router } = require("express");
 
-const posts = require('./posts')
-const users = require('./users')
-const comments = require('./comments')
-const tags = require('./tags')
-const modules = require('./modules')
-const postLikes = require('./like_posts')
-const commentLikes = require('./like_comments')
+const posts = require("./posts");
+const users = require("./users");
+const comments = require("./comments");
+const tags = require("./tags");
+const modules = require("./modules");
+const postLikes = require("./like_posts");
+const commentLikes = require("./like_comments");
+const loginUser = require("./login");
 
 const router = Router();
 
-router.use('/posts', posts)
-router.use('/users', users)
-router.use('/tags', tags)
-router.use('/modules', modules)
-router.use('/comments', comments)
-router.use('/post_likes', postLikes)
-router.use('/comment_likes', commentLikes)
+router.use("/posts", posts);
+router.use("/users", users);
+router.use("/tags", tags);
+router.use("/modules", modules);
+router.use("/comments", comments);
+router.use("/post_likes", postLikes);
+router.use("/comment_likes", commentLikes);
+router.use("/login_user", loginUser);
 
-
-router.use('/', (req, res) => {
-    res.status(200).send({message: "Ruta principal conectada exitosamente"})
-})
+router.use("/", (req, res) => {
+  res.status(200).send({ message: "Ruta principal conectada exitosamente" });
+});
 
 module.exports = router;
